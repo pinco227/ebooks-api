e37dfd088da92c2275f270f0b884ec0b411c092b
Paul Istratoaie <p4ul.gg@gmail.com>
2021-07-14 19:08:38 +0100

    Add concrete API views and add related urls

--- ebooks/api/urls.py
+++ ebooks/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from ebooks.api.views import EbookListCreateAPIView
+from ebooks.api.views import EbookListCreateAPIView, EbookDetailAPIView
 
 urlpatterns = [
     path("ebooks/", EbookListCreateAPIView.as_view(), name="ebook-list"),
+    path("ebooks/<int:pk>", EbookDetailAPIView.as_view(), name="ebook-detail"),
 ]
--- ebooks/api/views.py
+++ ebooks/api/views.py
@@ -1,18 +1,28 @@
-from rest_framework import generics, mixins
+from rest_framework import generics
+# from rest_framework import mixins
 
 from ebooks.models import Ebook
 from ebooks.api.serializers import EbookSerializer
 
 
-class EbookListCreateAPIView(mixins.ListModelMixin,
-                             mixins.CreateModelMixin,
-                             generics.GenericAPIView):
-
+class EbookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Ebook.objects.all()
     serializer_class = EbookSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+class EbookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ebook.objects.all()
+    serializer_class = EbookSerializer
+
+# class EbookListCreateAPIView(mixins.ListModelMixin,
+#                              mixins.CreateModelMixin,
+#                              generics.GenericAPIView):
+
+#     queryset = Ebook.objects.all()
+#     serializer_class = EbookSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
